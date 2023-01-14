401900951860e44d4d9fc0c3cbbbb2da7cb03f55
Nizar8055 <104364273+Nizar8055@users.noreply.github.com>
2023-01-14 23:12:58 +0530

    Add files via upload

---
+++ storybook/src/stories/Tablr.stories.js
@@ -0,0 +1,10 @@
+import { tablr } from '../Tablr';
+
+export default {
+  title: 'Table - A Customizable React Table Component',
+  component: tablr,
+};
+
+const Template = args => <tablr />;
+
+export const Default = Template.bind({});
